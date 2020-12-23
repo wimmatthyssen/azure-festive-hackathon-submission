213b0cdf9d0302229bed27d853ff34318e4e039b
Wim Matthyssen <62010638+wimmatthyssen@users.noreply.github.com>
2020-12-23 16:42:58 +0100

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "santawishlist-tf-state-rg"
-    storage_account_name = "<my-storage-account-name>"
+    storage_account_name = "santawishlisttstate30151"
     container_name       = "tfstate"
     key                  = "santawishlist.terraform.tfstate"
   }
